de062bba2068567e2c7fb931f646237a6af5c6ee
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2024-02-15 12:51:28 -0500

    Conversor de moneda 1.0.0
    Se Definen los parametros para que el usuario pueda ingresar un valor en pesos y mediante la tasa de cambio ajustada a 4800  para despues mostrar en pantalla el resultado de la conversion

---
+++ CoversorDeMoneda.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package coversor.de.moneda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sandra Verano
+ */
+public class CoversorDeMoneda {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);// Para que el codigo tome los inputs del usuario
+        
+        double TasadeCambioP_D = 4800.0;// Se establece la tabla de cambio de peso Colombiano a dolar
+        
+        System.out.println("Ingrese el valor en pesos Colombianos: ");
+        double pesosColombianos = scanner.nextDouble();
+        dsafasdfdasd
+                 
+    }
+    
+}
